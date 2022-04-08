da88a21e2806608c7b7c64859bf4c20a6c6deb40
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-04-08 23:14:13 +0400

    Initial commit

---
+++ CmdCommand/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CmdCommand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string command = "ipconfig";
+            string command = "ping 192.168.1.254";
+            string result = ExecuteCmd.ExecuteCommandSync(command);
+            Console.WriteLine(result);
+        }
+        //static string Command(string CmdCommand)
+        //{
+
+
+        //    Process CommandProcess = new Process();
+        //    CommandProcess.StartInfo.FileName = "cmd.exe";
+        //    CommandProcess.StartInfo.UseShellExecute = false;
+        //    CommandProcess.StartInfo.Arguments = CmdCommand;
+        //    CommandProcess.StartInfo.RedirectStandardOutput = true;
+        //    CommandProcess.Start();
+        //    return CommandProcess.StandardOutput.ReadToEnd();
+
+        //}
+    }
+}
